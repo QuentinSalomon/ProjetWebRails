1734080fa3ba0e6e3582480cddeba742cacff00f
Luc Aebischer <aebiluc@gmail.com>
2016-04-26 16:06:08 +0200

    Creation des scaffold station et type

--- Station/config/routes.rb
+++ Station/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :gas_types
+  resources :gas_stations
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
