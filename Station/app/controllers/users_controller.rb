00e683254e77d30a5db5e631c3a922a7c25a3e0d
QuentinSalomon <q.salomon@bluewin.ch>
2016-05-11 13:58:15 +0200

    suppression des liens de connexion

--- Station/app/controllers/users_controller.rb
+++ Station/app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    authorize! :read, @user, :message => "Vous n'avez pas l'autorisation"
   end
 
   # GET /users/new
