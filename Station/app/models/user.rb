32c069a4f885310f66ab7b7be5605999ce1f386d
QuentinSalomon <q.salomon@bluewin.ch>
2016-05-31 16:24:59 +0200

    role user à la création

--- Station/app/models/user.rb
+++ Station/app/models/user.rb
@@ -1,11 +1,18 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :roles
   has_many :vehiculs
+  before_save(:save_role_user)
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+ def save_role_user
+   if !has_role?(:user)
+     self.roles << Role.find_by(:name => 'user')
+   end
+ end
 
   def has_role?(role)
     return self.roles.find_by(:name => role.to_s.camelize)
